93b372aeb60efb9303f615a894b36a8e8e0f573f
Andy <andy@Ais-MacBook-Pro.local>
2024-12-25 07:40:57 +0900

    fix bug

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,16 @@
-import aboutImg from '../images/about.jpeg';
-import Title from './Title';
+import aboutImg from '../images/about.jpeg'
+import Title from './Title'
 
 const About = () => {
   return (
-    <section className='section' id='about'>
-      <Title title='about' subTitle='us' />
+    <section className="section" id="about">
+      <Title title="about" subTitle="us" />
 
-      <div className='section-center about-center'>
-        <div className='about-img'>
-          <img src={aboutImg} className='about-photo' alt='awesome beach' />
+      <div className="section-center about-center">
+        <div className="about-img">
+          <img src={aboutImg} className="about-photo" alt="awesome beach" />
         </div>
-        <article className='about-info'>
+        <article className="about-info">
           <h3>explore the difference</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
@@ -22,12 +22,12 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href='#' className='btn'>
+          <button onClick={() => console.log('TEST')} className="btn">
             read more
-          </a>
+          </button>
         </article>
       </div>
     </section>
-  );
-};
-export default About;
+  )
+}
+export default About
